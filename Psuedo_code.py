c24d8f41245968a0c0641792f57c8a67db272fd0
Vishal Sharma <vishal.473079@gmail.com>
2022-03-08 21:20:29 +0530

    first commit

---
+++ Psuedo_code.py
@@ -0,0 +1,23 @@
+'''
+Today, I am here to code for Hangman game.
+Following logics I applyied in this game:
+
+1. Using the random library
+2. Making an input variable for the user name and saying hi
+3. Making a list of words
+4. Creating a variable that randomly choose words from out previous list
+5. Telling the users to guess the characters.
+6. Creating a guessing variable.
+7. Creating a turns variable and setting it to any number.
+8. Again crating a while loop for turns while greater than 0
+9. Creating a variable in the while loop and call it failed and it equals to 0.
+10. Creating a for loop inside the while loop 
+11. Creating an if stmt in the for loop then print it, else print "_" and increase the failed varaible by one.
+12. Creating an if statement inside the while loop for failed and setting it to 0 and if its true print you win otherwise print word and break
+13. Create an input ion the guess variable to guess the character.
+14. Store every input characters in the guesses variable.
+15. Create an if stmt if guesses not in word then reduce "turns" by one and print wrong and print the number of turns left for the user.
+16. if turns=0
+you lose
+'''
+
